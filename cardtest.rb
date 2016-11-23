80297e75a478dc58be1794581276df2ea591d719
WillOB <obrienwp@s.dcsdk12.org>
2016-11-23 12:53:00 -0700

    Get crazy eights to mostly work for one player

--- cardtest.rb
+++ cardtest.rb
@@ -5,3 +5,7 @@ deck = Deck.new
 newdeck = deck.newDeck
 puts newdeck[0].rank
 puts newdeck[0].suit
+newdeck.each do |card|
+  puts card.rank
+  puts card.suit
+end
--- crazy8s.rb
+++ crazy8s.rb
@@ -7,10 +7,11 @@ p1 = deck[0..4]
 p2 = deck[5..9]
 discard = [deck[10]]
 deck = deck.drop(11)
+num = 10
 
 while p1.length != 0 && p2.length != 0
 
-faceUp = discard[0]
+faceUp = discard[-1]
 
 suit = faceUp.suit
 rank = faceUp.rank
@@ -18,42 +19,55 @@ rank = faceUp.rank
 puts "The card is the #{rank} of #{suit}."
 puts "Your cards are:"
 num = 1
-p1.each do |x|
-  puts "#{num}: #{x.rank} of #{x.suit}"
+p1.each do |card|
+  puts "#{num}: #{card.rank} of #{card.suit}"
   num += 1
 end
 
 matches = 0
-p1.each do |x|
-  if x.rank == 8
+p1.each do |card|
+  if card.rank == 8
     matches += 1
-  elsif suit == x.suit || rank == x.rank
+  elsif suit == card.suit || rank == card.rank
     matches += 1
   end
-
 end
 
-
 if matches > 0
-  print "Which card will you play?"
-  pick = scan
+  print "Which card will you play? >"
+  pick = scan.to_i
 else
   puts "You cannot play anything. Draw a card."
 end
 
 while pick < 1 || pick > 5
-  #tell them no
-if pickRank == 8
-  #dostuff
-elsif pickRank == rank || pickSuit == suit
-  #do other stuff
-else
-  puts "Nuh uh! Pick another card."
+  print "Please enter the number of the card you want to play. >"
+  pick = scan.to_i
 end
 
+pickRank = p1[pick - 1].rank
+pickSuit = p1[pick - 1].suit
 
+while !(pickRank == rank || pickSuit == suit || pickRank == 8)
+    print "You can't play that card! Pick another. >"
+    pick = scan.to_i
+    pickRank = p1[pick - 1].rank
+    pickSuit = p1[pick - 1].suit
+end
 
-
+if pickRank == 8
+  print "Pick a new suit. > "
+  newSuit = scan.downcase!
+  discard.push(Card.new(pickRank, pickSuit))
+  p1.delete_at(pick-1)
+  num += 1
+  p1.push(deck[num])
+elsif pickRank == rank || pickSuit == suit
+  discard.push(Card.new(pickRank, pickSuit))
+  p1.delete_at(pick-1)
+  num += 1
+  p1.push(deck[num])
+end
 
 
 end
